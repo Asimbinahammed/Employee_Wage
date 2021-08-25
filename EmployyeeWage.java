1c44ae84e0fe3c0bd9194dad847971eaec85cdbb
asimbinahammed <asim242370@gmail.com>
2021-08-25 12:13:23 +0530

    UC5 calculating wage for month

--- EmployyeeWage.java
+++ EmployyeeWage.java
@@ -2,35 +2,34 @@ package bridgelabz;
 
 public class EmployyeeWage {
 	
-		public static final int IS_FULL_TIME=2;
-		public static final  int IS_PART_TIME =1;
-		public static final  int EMP_RATE_PER_HOUR =20;
-		
-		public static void main(String[] args) {
-			int empCheck =(int) Math.floor(Math.random()*10)%3;
-			EmployyeeWage obj=new EmployyeeWage();
-			obj.wageCalc(empCheck);
-		}	
-		
-		
-		public void wageCalc(int empCheck) {
+	public static final int IS_FULL_TIME=2;
+	public static final  int IS_PART_TIME =1;
+	public static final  int EMP_RATE_PER_HOUR =8;
+	public static final  int Working_Days =20;
+	
+	//main method
+	public static void main(String[] args) {
+		int empCheck =(int) Math.floor(Math.random()*10)%3;
+		EmployyeeWage obj=new EmployyeeWage();
+		obj.workingDays(empCheck);
+	}
+		//calculation of total wage
+		public void workingDays(int empCheck) {
 			int wage=0;
 			int empHours=0;
-			switch (empCheck) {
-			case IS_FULL_TIME:
-				 empHours=8;
-				break;
-			
-			case IS_PART_TIME:
-				 empHours=4;
-				break;
-				
-				default:
-					empHours=0;
-			}
-			wage=empHours*EMP_RATE_PER_HOUR;
-			System.out.println("Emp wage :" +wage);
+		switch (empCheck) {
+		case IS_FULL_TIME:
+			 empHours=8;
+			break;
 		
+		case IS_PART_TIME:
+			 empHours=4;
+			break;
+			
+			default:
+				empHours=0;
 		}
-
+		wage=empHours*EMP_RATE_PER_HOUR*Working_Days;
+		System.out.println("Emp wage :" +wage);
+	}
 }
