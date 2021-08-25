75c909810732cc8dec597b8fa6a781249dac4a52
asimbinahammed <asim242370@gmail.com>
2021-08-25 12:04:18 +0530

    Wage calculation

--- EmployyeeWage.java
+++ EmployyeeWage.java
@@ -1,23 +1,35 @@
 package bridgelabz;
 
 public class EmployyeeWage {
-
+	
 	//checking method
-		public void empPresntCheck() {
-			double empCheck = Math.floor(Math.random()*10)%2;
-			if(empCheck==0) {
-				System.out.println("Employee Present");
+			public double empPresntCheck() {
+				double empCheck = Math.floor(Math.random()*10)%2;
+				if(empCheck==0) {
+					System.out.println("Employee Present");
+				}
+				else {
+					System.out.println("Employee absent");
+				}
+				return empCheck;
 			}
-			else {
-				System.out.println("Employee absent");
-			}
-		}
-		
-		//main method
-		public static void main(String[] args) {
-			EmployyeeWage obj=new EmployyeeWage();
-			obj.empPresntCheck();
 			
-		}
+			//uc2 calculation of wage 
+			public void dailyWageEmployee(double check) {
+				int wage=20;
+				int hour=8;
+				if(check==0) {
+					int dailyWage=wage*hour;
+					System.out.println("Daily wage : "+ dailyWage);
+				}
+			}
+					
+			//main method
+			public static void main(String[] args) {
+				EmployyeeWage obj=new EmployyeeWage();
+				double check=obj.empPresntCheck();
+				obj.dailyWageEmployee(check);
+			
+			}
 
 }
